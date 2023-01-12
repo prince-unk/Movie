78eb0c89dc6089327d275674b8204ce2d3ff97c9
prince-unk <105361236+prince-unk@users.noreply.github.com>
2023-01-12 23:14:46 +0530

    Update

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({movie}) => {
     return (
         <>
             <div className='card_main'>
-                <div className='top'><h2>{}</h2></div>
+                <div className='top'><h2>{movie.title}</h2></div>
                 <div className='middle'></div>
                 <div className='bottom'><h4>{}</h4></div>
             </div>
--- client/src/Components/Movies.jsx
+++ client/src/Components/Movies.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Card from './Card'
 
 const Movies = () => {
+    const [result, setresult] = useState([]);
     const [searchMovie, setsearchmovie] = useState("");
     let searchvalue;
     const handleInputChange = (A) => {
@@ -10,7 +11,7 @@ const Movies = () => {
         // console.log(searchvalue);
         setsearchmovie(searchvalue);
 
-        console.log(searchMovie);
+        // console.log(searchMovie);
     }
     const options = {
         method: 'GET',
@@ -20,15 +21,23 @@ const Movies = () => {
         }
     };
 
-    let url1 = 'https://online-movie-database.p.rapidapi.com/auto-complete?q=';
+    let url1 = 'https://api.themoviedb.org/3/search/movie?api_key=58e4b615a86a14cebb48ad1e3c73d310&language=en-US&query=';
     let url2 = searchMovie;
-     let url = url1 + url2;
+    let url = url1 + url2;
 
+    const fetchdata = async () => {
+        const DATA = await fetch(url)
+        const Moviedata = await DATA.json();
+        // console.log(Moviedata);
+        setresult(Moviedata.results);
 
-    fetch(url  , options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    }
+    console.log(result);
+
+    // fetch(url  , options)
+    //     .then(response => response.json())
+    //     .then(response => console.log(response))
+    //     .catch(err => console.error(err));
     return (
         <>
             <div className='main'>
@@ -36,19 +45,17 @@ const Movies = () => {
                     <form>
                         <input type="text" id="filter" placeholder="Search" onChange={handleInputChange} />
                     </form>
+                    <button onClick={fetchdata}>Search</button>
                 </div>
                 <div className='middle'>
-                   {
-                    response.d.map((el)=>{
-                     return(
-                        <>
-                        
-                        </>
-                     )
-
+                    {result.map((movie) => {
+                        return (
+                            <>
+                            <Card key={movie.id} movie={movie} />
+                            </>
+                        )
                     })
-                   }
-                    <Card />
+                    }
                 </div>
             </div>
         </>
